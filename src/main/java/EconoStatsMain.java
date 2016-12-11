74a8884ced7faf805b3e34015d4079cb7ccc2868
stenman <roger.stenman@gmail.com>
2016-12-11 14:02:42 +0100

    Added support for Spring Boot Added logger

--- src/main/java/EconoStatsMain.java
+++ src/main/java/EconoStatsMain.java
@@ -1,9 +1,28 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * The main class of this application
  */
 public class EconoStatsMain {
     public static void main(String args[]) {
-        System.out.println("Hello, world!");
+
+        final Logger LOGGER = LoggerFactory.getLogger(EconoStatsMain.class);
+
+        //TODO: Make this dynamic
+        final String CSV_FILE = "c:/development/testdata/export.csv";
+
+        try {
+            FileReader fr = new FileReader(CSV_FILE);
+            BufferedReader br = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            LOGGER.debug("Add error message");
+            e.printStackTrace();
+        }
 
     }
 }
