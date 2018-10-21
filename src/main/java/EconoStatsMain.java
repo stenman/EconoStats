2877796672ad137eca9b5f946ced6cd035b0ec3d
stenman <roger.stenman@gmail.com>
2018-10-21 15:12:26 +0200

    Removed some unnecessary comments Added list in main

--- src/main/java/EconoStatsMain.java
+++ src/main/java/EconoStatsMain.java
@@ -1,22 +1,8 @@
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderHeaderAware;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.lang.Integer.valueOf;
 
 /**
  * The main class of this application
@@ -34,13 +20,15 @@ public class EconoStatsMain {
         //TODO: (later/bonus) insert all objects into DB
         //TODO: remove all objects from list that do not exist in premade list of names (don't forget to LOG the removals!)
         //TODO: now we should have a list of all transactions that should be in the new CSV
+        List<AccountTransaction> accountTransactions = new ArrayList<>();
         NordeaCsvReader nordeaCsvReader = new NordeaCsvReader();
         try {
-            nordeaCsvReader.parseCsv(CSV_FILE, ",", new char[]{'"'});
+            accountTransactions = nordeaCsvReader.parseCsv(CSV_FILE, ",", new char[]{'"'});
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+//        DatabaseConnector.createTables();
 
         //TODO: 2. create new csv
         //TODO: create headers
@@ -52,21 +40,5 @@ public class EconoStatsMain {
         //TODO: check DB for identical transactions --> don't add those to csv, but add all others
 
         //TODO: 4. find a good statistics tool and display some nice stats... start with a pie chart!
-
-//        DatabaseConnector.createTables();
-
-//        try {
-//            FileReader fr = new FileReader(CSV_FILE);
-//            BufferedReader br = new BufferedReader(fr);
-//            while (br.readLine() != null) {
-//                System.out.println(br.readLine());
-//            }
-//        } catch (FileNotFoundException e) {
-//            LOGGER.debug("Add error message");
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
     }
 }
