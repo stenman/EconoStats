f8d262ef406b51fca8d02bd06447b386fde06ebb
stenman <roger.stenman@gmail.com>
2018-11-11 13:52:55 +0100

    Changed todo list

--- src/main/java/se/perfektum/econostats/EconostatsMain.java
+++ src/main/java/se/perfektum/econostats/EconostatsMain.java
@@ -9,20 +9,18 @@ public class EconostatsMain {
     // Done Tested
     // [x]  [x] read CSV
     // [x]  [x] create object for each transaction in CSV, put each object in List<accountTransaction>
-    // [ ]  [ ] list all files in /EconoStats folder
-    //          if AccountTransactions.json exists:
-    // [ ]  [ ]     get existing JSON from /EconoStats folder (if exists)
-    // [ ]  [ ]     convert existing JSON into another List<AccountTransaction>
-    // [ ]  [ ]     compare new and old List<AccountTransaction>:
-    // [ ]  [ ]          merge the two lists (insert (into a "full" list) all objects that are not redundant)
-    // [ ]  [ ]          create a full JSON from the full list
-    // [ ]  [ ]     create/save the full JSON to drive
-    // [ ]  [ ]         create "EconoStats" folder (if "EconoStats" folder did not exist)
-    // [ ]  [ ]         overwrite old file
-    // [ ]  [ ]     remove all objects from list that do not exist in premade configuration list of names
-    // [ ]  [ ]     use the new list to create ODF file
-    // [ ]  [ ]     save ODF file to "EconoStats" folder
-    //          else, just save the loaded AccountTransation list to drive
+    // [ ]  [ ] get existing JSON from /EconoStats folder (if exists)
+    // [ ]  [ ]     if it doesn't exist, just create a new ODF from input file and save it to GDrive
+    // [ ]  [ ] convert existing JSON into another List<AccountTransaction>
+    // [ ]  [ ] compare new and old List<AccountTransaction>:
+    // [ ]  [ ]      merge the two lists (insert (into a "full" list) all objects that are not redundant)
+    // [ ]  [ ]      create a full JSON from the full list
+    // [ ]  [ ] create/save the full JSON to drive
+    // [ ]  [ ]     create "EconoStats" folder (if "EconoStats" folder did not exist)
+    // [ ]  [ ]     overwrite old file
+    // [ ]  [ ] remove all objects from list that do not exist in premade configuration list of names
+    // [ ]  [ ] use the new list to create ODF file
+    // [ ]  [ ] save ODF file to "EconoStats" folder
     //
     // [ ]  [ ] future: LOG all removals etc.!
     // [ ]  [ ] future: use config file, save in "EconoStats" folder
