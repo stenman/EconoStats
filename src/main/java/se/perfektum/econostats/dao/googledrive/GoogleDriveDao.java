3b3efd6bfdc5c47395c797e1711be001d2d04aa9
stenman <roger.stenman@gmail.com>
2019-01-06 13:25:30 +0100

    Added logging

--- src/main/java/se/perfektum/econostats/dao/googledrive/GoogleDriveDao.java
+++ src/main/java/se/perfektum/econostats/dao/googledrive/GoogleDriveDao.java
@@ -15,6 +15,9 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.perfektum.econostats.EconoStats;
 import se.perfektum.econostats.dao.AccountTransactionDao;
 
 import java.io.*;
@@ -26,14 +29,19 @@ import java.util.stream.Collectors;
 
 public class GoogleDriveDao implements AccountTransactionDao {
 
+    final Logger LOGGER = LoggerFactory.getLogger(GoogleDriveDao.class);
+
     private static final String APPLICATION_NAME = "EconoStats";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String ACCESS_TYPE = "offline";
+    private static final String USER = "user";
+    private static final int PORT_NUMBER = 8888;
 
     public static final String APPLICATION_VND_GOOGLE_APPS_FOLDER = "application/vnd.google-apps.folder";
 
     /**
-     * Global instance of the scopes required by this quickstart.
+     * Global instance of the scopes required by this application.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
@@ -48,22 +56,28 @@ public class GoogleDriveDao implements AccountTransactionDao {
      */
     private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
+        LOGGER.trace(String.format("Loading local client secrets from file '%s'", CREDENTIALS_FILE_PATH));
         InputStream in = GoogleDriveDao.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        LOGGER.trace("Loading Google client secrets");
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
+        LOGGER.trace(String.format("Building Google Authorization Code Flow with the following parameters - SCOPES:[%s], Tokens Directory Path:[%s], Access Type:[%s]", SCOPES, TOKENS_DIRECTORY_PATH, ACCESS_TYPE));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
+                .setAccessType(ACCESS_TYPE)
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        LOGGER.trace(String.format("Building Local Server Receiver on port [%s]", PORT_NUMBER));
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(PORT_NUMBER).build();
+        LOGGER.trace(String.format("Authorizing user '%s'", USER));
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize(USER);
     }
 
-    public Drive getService() throws IOException, GeneralSecurityException {
+    private Drive getService() throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        LOGGER.trace(String.format("Building authorized Drive API client server for '%s'", APPLICATION_NAME));
         Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
@@ -76,6 +90,7 @@ public class GoogleDriveDao implements AccountTransactionDao {
         fileMetadata.setName(name);
         fileMetadata.setMimeType(APPLICATION_VND_GOOGLE_APPS_FOLDER);
 
+        LOGGER.debug(String.format("Creating folder on Google Drive: '%s'", name));
         File file = getService().files().create(fileMetadata)
                 .setFields("id")
                 .execute();
@@ -91,6 +106,7 @@ public class GoogleDriveDao implements AccountTransactionDao {
 
         FileContent mediaContent = new FileContent(fileContentMimeType, filePath);
 
+        LOGGER.debug(String.format("Creating file on Google Drive: filePath:[%s], parents:[%s], fileMimeType:[%s], fileContentMimeType:[%s]", filePath, parents, fileMimeType, fileContentMimeType));
         File file = getService().files().create(fileMetadata, mediaContent)
                 .setFields("id")
                 .execute();
@@ -99,6 +115,7 @@ public class GoogleDriveDao implements AccountTransactionDao {
 
     @Override
     public void updateFile(String fileId, java.io.File filePath, String fileContentMimeType) throws IOException, GeneralSecurityException {
+        LOGGER.debug(String.format("Fetching file from Google Drive - fileId:'%s'", fileId));
         File existingFile = getService().files().get(fileId).execute();
         File fileMetadata = new File();
         fileMetadata.setName(existingFile.getName());
@@ -107,6 +124,8 @@ public class GoogleDriveDao implements AccountTransactionDao {
 
         FileContent mediaContent = new FileContent(fileContentMimeType, filePath);
 
+        LOGGER.debug(String.format("Updating file on Google Drive with the following parameters - fileId:[%s], name:[%s], parents:[%s], mimeType:[%s], fileContentMimeType:[%s], filePath:[%s]"
+                , fileId, fileMetadata.getName(), fileMetadata.getParents(), fileMetadata.getMimeType(), fileContentMimeType, filePath));
         getService().files().update(fileId, fileMetadata, mediaContent).execute();
     }
 
@@ -114,6 +133,7 @@ public class GoogleDriveDao implements AccountTransactionDao {
     @Override
     public List<String> searchForFile(String name, String mimeType) throws IOException, GeneralSecurityException {
         String pageToken = null;
+        LOGGER.debug(String.format("Searching for file on Google Drive - name:'%s', mimeType:'%s'", name, mimeType));
         List<String> items = new ArrayList<>();
         do {
             FileList result = getService().files().list()
@@ -126,12 +146,16 @@ public class GoogleDriveDao implements AccountTransactionDao {
             pageToken = result.getNextPageToken();
         } while (pageToken != null);
 
+        if (items.isEmpty()) {
+            LOGGER.debug(String.format("File not found on Google Drive - name:'%s', mimeType:'%s'", name, mimeType));
+        }
         return items;
     }
 
     @Override
     public String getFile(String fileId) throws IOException, GeneralSecurityException {
         OutputStream outputStream = new ByteArrayOutputStream();
+        LOGGER.debug(String.format("Downloading file from Google Drive - fileId:'%s'", fileId));
         getService().files().get(fileId)
                 .executeMediaAndDownloadTo(outputStream);
         return outputStream.toString();
